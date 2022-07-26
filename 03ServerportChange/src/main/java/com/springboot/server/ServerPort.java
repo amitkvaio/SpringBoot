2e42a769a1d0ae4932800cb8e7f2a3835eb4b0c1
amitkvaio <amitkvaio@gmail.com>
2022-07-26 08:46:44 +0530

    Changing the version of spring boot and other minor config changes
    Changing the version of spring boot and other minor config changes

--- 03ServerportChange/src/main/java/com/springboot/server/ServerPort.java
+++ 03ServerportChange/src/main/java/com/springboot/server/ServerPort.java
@@ -1,20 +1,15 @@
 package com.springboot.server;
 
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ServerPort implements EmbeddedServletContainerCustomizer {
+public class ServerPort implements TomcatConnectorCustomizer {
 
 	@Override
-	public void customize(ConfigurableEmbeddedServletContainer container) {
+	public void customize(Connector connector) {
 		//to change the port
-		container.setPort(2050);
-		
-		//to change the contextPath.
-		container.setContextPath("/serverportChange");
-	
+		connector.setPort(2050);
 	}
-	
 }
